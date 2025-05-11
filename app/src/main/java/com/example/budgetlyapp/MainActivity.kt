811e13cc92a915fe21aeab794f5edd9b1b04895d
lucasRivera21 <lucasriverar2001@gmail.com>
2025-05-10 20:21:17 -0500

    NEW FEATURE: Material theme configuration

--- app/src/main/java/com/example/budgetlyapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetlyapp/MainActivity.kt
@@ -6,20 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.budgetlyapp.ui.theme.BudgetlyAppTheme
+import com.example.budgetlyapp.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BudgetlyAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AppTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -34,14 +35,15 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.primary
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BudgetlyAppTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
