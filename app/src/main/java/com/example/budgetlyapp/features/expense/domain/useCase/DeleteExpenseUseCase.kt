0b22fa1447a99343c2fbd17fae882fcc307e0f35
lucasRivera21 <lucasriverar2001@gmail.com>
2025-08-13 00:16:02 -0500

    NEW FEATURE: Add cancel notification in expense screen when expense is deleted

--- app/src/main/java/com/example/budgetlyapp/features/expense/domain/useCase/DeleteExpenseUseCase.kt
+++ app/src/main/java/com/example/budgetlyapp/features/expense/domain/useCase/DeleteExpenseUseCase.kt
@@ -1,10 +1,21 @@
 package com.example.budgetlyapp.features.expense.domain.useCase
 
+import com.example.budgetlyapp.alarm.AlarmScheduler
 import com.example.budgetlyapp.features.expense.data.repository.ExpenseRepository
 import javax.inject.Inject
 
-class DeleteExpenseUseCase @Inject constructor(private val expenseRepository: ExpenseRepository) {
+class DeleteExpenseUseCase @Inject constructor(
+    private val expenseRepository: ExpenseRepository,
+    private val alarmScheduler: AlarmScheduler
+) {
     suspend operator fun invoke(expenseId: String) {
+        val taskToUploadNotificationResponse = expenseRepository.getTaskList(expenseId)
+        taskToUploadNotificationResponse.forEach {
+            if (it.requestCode != null) {
+                alarmScheduler.cancel(it.requestCode)
+            }
+        }
+
         expenseRepository.deleteExpense(expenseId)
     }
 }
