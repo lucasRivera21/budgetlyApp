660d9cbd76d81dfe6226896d5f107b7a0ba6cbc3
lucasRivera21 <lucasriverar2001@gmail.com>
2025-08-15 14:37:02 -0500

    BUG FIXED: Fix issue to login

--- app/src/main/java/com/example/budgetlyapp/features/login/presentation/LoginViewModel.kt
+++ app/src/main/java/com/example/budgetlyapp/features/login/presentation/LoginViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.budgetlyapp.features.login.domain.LoginUserUseCase
+import com.example.budgetlyapp.features.splash.domain.useCase.FetchUserInfoUseCase
 import com.example.budgetlyapp.navigation.MainScreen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -19,7 +20,8 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUserUseCase: LoginUserUseCase,
-    @ApplicationContext private val context: Context
+    @ApplicationContext private val context: Context,
+    private val fetchUserInfoUseCase: FetchUserInfoUseCase
 ) :
     ViewModel() {
     private val _email = MutableStateFlow(TextFieldValue(""))
@@ -43,6 +45,9 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.IO) {
             _isLoading.value = true
             val resultLogin = loginUserUseCase(email.value.text, password.value.text)
+            if (resultLogin.isSuccess) {
+                fetchUserInfoUseCase()
+            }
             withContext(Dispatchers.Main) {
                 if (resultLogin.isSuccess) {
                     navController.navigate(MainScreen.route)
